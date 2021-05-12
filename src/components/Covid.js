f4fa9118c4121800e42122de0ccb9a2a51120a4f
hagoodwin <hagoodwi@gmail.com>
2021-05-11 17:01:15 -0700

    add api call

--- src/components/Covid.js
+++ src/components/Covid.js
@@ -1,67 +1,46 @@
-import React from 'react';
+import React, { useEffect,useState } from 'react';
 
-class Covid extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      covids: [],
-    };
-  }
+export default function Covid(){
 
-  makeUSApiCall = () => {
-    fetch(`https://api.covidtracking.com/v1/us/current.json`)
-      .then(response => response.json())
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://corona.lmao.ninja/v2/states/")
+      .then(res => res.json())
       .then(
-        (jsonifiedResponse) => {
-          this.setState({
-            isLoaded: true,
-            covids: jsonifiedResponse
-            
-          });
-        })
-        .catch((error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        });
-  }
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
 
-  componentDidMount() {
-    this.makeUSApiCall()
-  }
-
-render() {
-  const { error, isLoaded, covids } = this.state;
   if (error) {
-    return <React.Fragment>Error: {error.message}</React.Fragment>;
+    return <div>Error: {error.message}</div>;
   } else if (!isLoaded) {
-    return <React.Fragment>Loading...</React.Fragment>;
+    return <div>Loading...</div>;
   } else {
-    console.log(covids[0].date)
     return (
-      
-      <React.Fragment>
-        <h1>Covid 19 Tracker</h1>
-        <ul>
-        <h2>United States:</h2>
-          {covids.map((covid, index) =>
-            <ul key={index}>
-              <h5><em>Last Updated: {covid.dateChecked} = dateChecked</em></h5>
-              <h4>New Positive Cases: {covid.positiveIncrease}</h4>
-              <h4>New Deaths: {covid.deathIncrease}</h4>
-              <h4>Deaths Per Minute: {covid.deathIncrease/1440}</h4>
-            </ul>
-          )}
-        </ul>
-          
-    </React.Fragment>
+      <ul>
+        {items.map(item => (
+          <li key={item.id}>
+            {item.state} {item.todayDeaths} 
+          </li>
+        ))}
+
+      </ul>
     );
-  }
-  
   }
 }
 
-export default Covid;
+
